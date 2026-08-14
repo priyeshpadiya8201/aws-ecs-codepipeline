da5f7187eb739a4aff86bb051299ebcbe11e24f7
priyeshpadiya8201 <120197647+priyeshpadiya8201@users.noreply.github.com>
2026-08-14 16:29:31 +0530

    Update server response version to 2.0

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
 res.writeHead(200, { 'Content-Type': 'text/plain' });
-res.end('Hello from ECS Fargate! Version: 1.0\n');
+res.end('Hello from ECS Fargate! Version: 2.0\n');
 });
 server.listen(3000, () => console.log('Server running on port 3000'));
